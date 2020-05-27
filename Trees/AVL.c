b9ae0218887f875dcf01210938c5cdbdc2753135
mountAP <ap44122@gmail.com>
2020-05-27 23:03:38 +0530

    Working on AVL: insert()

--- Trees/AVL.c
+++ Trees/AVL.c
@@ -6,9 +6,8 @@
 #define RIGHT_HEAVY -1
 
 typedef struct Node {
-    int data;
+    int data, height;
     struct Node *left, *right, *parent;
-    int bf; // balance factor
 } Node, AVLTree;
 
 Node* createNewNode(int data)
@@ -16,18 +15,22 @@ Node* createNewNode(int data)
     Node* n = (Node*)malloc(sizeof(Node));
     n->data = data;
     n->left = n->right = n->parent = NULL;
-    n->bf = 0;
+    n->height = 0;
 }
 
-int height(AVLTree* T)
+int height(AVLTree *T) {
+    return (T == NULL)? 0: T->height;
+}
+
+void updateHeight(Node* n)
 {
-    if (T == NULL)
-        return 0;
+    if (n) 
+        n->height = max(height(n->left), height(n->right));
+}
 
-    int hL = height(T->left);
-    int hR = height(T->right);
-
-    return 1 + (hL > hR) ? hL : hR;
+int balanceFactor(AVLTree* T)
+{
+    return height(T->left) - height(T->right);
 }
 
 Node* find(AVLTree* T, int data)
@@ -44,12 +47,53 @@ AVLTree* insert(AVLTree* T, int data)
         T = createNewNode(data);
 
     else {
-        Node* cur = T, *par = NULL;
+        
+        Node *cur = T, *par = NULL;
         while (cur) {
             par = cur;
             cur = (data < cur->data) ? cur->left : cur->right;
         }
+
+        Node* newNode = createNewNode(data);
+        newNode->parent = par;
+        if (data < par->data)
+            par->left = newNode;
+        else
+            par->right = newNode;
+
+        updateHeight(par);
+
+        // climb upwards to look for critical nodes
+        cur = par->parent;
+        Node *child = par;
+        Node *childOfChild = newNode;
         
+        while (cur) {
+            // calculate the balance factor of current
+            int bf = balanceFactor(cur);
+            
+            // if current is a critical node
+            if (bf > 1 || bf < -1) {
+                // determine the type of rotation needed 
+                // to rebalance and then rotate
+                if (childOfChild == cur->left->left) {
+                    // LL
+                    
+                } else if (childOfChild == cur->right->right) {
+                    // RR
+                } else if (childOfChild == cur->left->right) {
+                    // LR
+                } else if (childOfChild == cur->right->left) {
+                    // RL
+                }
+            }
+            
+            // move up
+            childOfChild = child;
+            child = cur;
+            cur = cur->parent;
+            
+        }
     }
 
     return T;
