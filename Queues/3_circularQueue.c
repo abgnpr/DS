c5d6c7b09f4b0172cd36ef11ea601241e6525cae
mountAP <ap44122@gmail.com>
2020-07-02 21:28:55 +0530

    documented 3_circularQueue.c

--- Queues/3_circularQueue.c
+++ Queues/3_circularQueue.c
@@ -15,6 +15,7 @@ typedef struct Queue {
     int arr[];
 } Queue;
 
+// creates a new queue of size 'max'
 Queue* createQueue(int max)
 {
     Queue* Q = malloc(sizeof(Queue) + sizeof(int) * max);
@@ -24,55 +25,72 @@ Queue* createQueue(int max)
     return Q;
 }
 
+// returns the size of the queue
 int size(Queue* Q)
 {
+    // queue has 0 elements when head
+    // and tail are at initial position
     if (Q->tail == -1 && Q->head == 0)
         return 0;
 
+    // number of elements from head to tail
     int diff = Q->tail - Q->head + 1;
+    // result +ve (head is on the left of tail), it is the size
+    // result -ve (tail is on the left of head), size is max - |result| = max + result
     return (Q->tail >= Q->head) ? diff : Q->max + diff;
 }
 
+// returns true if the queue is empty
 int queueEmpty(Queue* Q) { return (size(Q) == 0) ? TRUE : FALSE; }
 
+// returns true if the queue if full
 int queueFull(Queue* Q) { return (size(Q) == Q->max) ? TRUE : FALSE; }
 
+// adds a data element to the queue end
 void enqueue(Queue* Q, int data)
 {
     if (queueFull(Q))
         printf("\033[31mOverflow\033[0m");
 
     else {
-        if (Q->tail == Q->max - 1)
-            Q->tail = 0;
-        else
-            Q->tail += 1;
+        // reset the tail to pos:0 using % when
+        // it reaches the end of the array;
+        // otherwise just increment it by 1
+        Q->tail = (Q->tail + 1) % Q->max;
+
+        // alternate
+        // Q->tail = (Q->tail == Q->max - 1)? 0 : Q->tail + 1;
 
         Q->arr[Q->tail] = data;
     }
 }
 
+// removes a data element from the queue front
 void dequeue(Queue* Q)
 {
     if (queueEmpty(Q))
         printf("\033[31mUnderflow\033[0m");
 
     else {
-        // print instead of return
+        // print de-queued
         printf("\nDequeued: %d\n", Q->arr[Q->head]);
 
+        // coincidence of head and tail implies
+        // that the queue has become empty, so
+        // we reset both head and tail.
         if (Q->head == Q->tail) {
             Q->head = 0;
             Q->tail = -1;
 
-        } else if (Q->head == Q->max - 1)
-            Q->head = 0;
-
-        else
-            Q->head += 1;
+        } else
+            // reset the head to pos:0 using % when
+            // it reaches the end of the array;
+            // otherwise just increment it by 1
+            Q->head = (Q->head + 1) % Q->max;
     }
 }
 
+// shows the element at the queue front
 void peekFront(Queue* Q)
 {
     if (queueEmpty(Q))
@@ -82,6 +100,7 @@ void peekFront(Queue* Q)
         printf("\nFront(HEAD): %d\n", Q->arr[Q->head]);
 }
 
+// shows the element at the queue end
 void peekBack(Queue* Q)
 {
     if (queueEmpty(Q))
@@ -91,6 +110,7 @@ void peekBack(Queue* Q)
         printf("\nBack(TAIL): %d\n", Q->arr[Q->tail]);
 }
 
+// prints the queue
 void print(Queue* Q)
 {
     if (queueEmpty(Q))
