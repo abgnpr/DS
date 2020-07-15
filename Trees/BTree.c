12fdadc8dad60ac6f3bf8c04460d339dbf8608d7
mountAP <ap44122@gmail.com>
2020-07-15 12:20:49 +0530

    Init

--- Trees/BTree.c
+++ Trees/BTree.c
@@ -54,6 +54,7 @@ int search(BTree* BT, int key, Node** node)
     *node = BT->root;
 
     for (;;) {
+
         int L = 0, R = (*node)->nKeys - 1, mid;
         while (L <= R) {
             mid = (L + R) / 2;
@@ -74,6 +75,119 @@ int search(BTree* BT, int key, Node** node)
     return NOT_FOUND;
 }
 
+/*
+typedef struct SearchResult {
+    Node* node;
+    int index;
+} SearchResult;
+
+int searchV2(BTree* BT, int key, Node** node)
+{
+    if (BT->root == NULL) {
+        *node = NULL;
+        return NOT_FOUND;
+    }
+
+    *node = BT->root;
+
+    for (;;) {
+        
+        int L = 0, R = (*node)->nKeys - 1, mid;
+        while (L <= R) {
+            mid = (L + R) / 2;
+            if (key == (*node)->keys[mid])
+                return mid;
+            else if (key < (*node)->keys[mid])
+                R = mid - 1;
+            else
+                L = mid + 1;
+        }
+
+        if ((*node)->ptrs[L] != NULL)
+            *node = (*node)->ptrs[L];
+        else
+            break;
+    }
+
+    return NOT_FOUND;
+}
+*/
+
+void delKey(Node* node, int index)
+{
+    for (int i = index + 1; i < node->nKeys; ++i)
+        node->keys[i - 1] = node->keys[i];
+
+    node->nKeys -= 1;
+}
+
+void insKey(Node* node, int key)
+{
+}
+
+void del(BTree* BT, int key)
+{
+    if (BT->root == NULL) {
+        printf("%d doesn't exist.\n", key);
+        return;
+    }
+
+    // node from which the key is to be deleted
+    Node* node = BT->root;
+    int d, // index of the key to be deleted
+        p; // index of node in the parent's ptrs[]
+
+    for (;;) {
+
+        int L = 0, R = node->nKeys - 1, mid;
+        while (L <= R) {
+            mid = (L + R) / 2;
+            if (key == node->keys[mid]) {
+                d = mid;
+                break;
+            } else if (key < node->keys[mid])
+                R = mid - 1;
+            else
+                L = mid + 1;
+        }
+
+        // note: make sure we set all values in
+        // ptrs[] of all leaves to NULL, otherwise
+        // the following step will produce undefined
+        // results
+
+        if (node->ptrs[L] != NULL) {
+            p = L;
+            node = node->ptrs[L];
+        } else
+            break;
+    }
+
+    if (node->nKeys > (M - 1) / 2)
+        delKey(node, d);
+
+    else {
+        Node *LSib = node->parent->ptrs[p - 1], // left sibling
+             *RSib = node->parent->ptrs[p + 1]; // right sibling
+
+        if (LSib && LSib->nKeys > (M - 1) / 2) {
+            insKey(node, node->parent->keys[p -1]);
+            node->parent->keys[p - 1] = LSib->keys[LSib->nKeys - 1];
+            delKey(LSib, LSib->nKeys - 1);
+        }
+
+        else if (RSib && RSib->nKeys > (M - 1) / 2) {
+            insKey(node, node->parent->keys[p]);
+            node->parent->keys[p] = RSib->keys[0];
+            delKey(RSib, 0);
+        }
+
+        else {
+            
+        }
+    }
+}
+
 void swapKeys(int A[], int m, int n)
 {
     int temp = A[m];
@@ -88,7 +202,6 @@ void swapPtrs(Node** P, int m, int n)
     P[n] = temp;
 }
 
-// issue - pointer management
 void ins(BTree* BT, int key)
 {
     // if the tree is empty create a new node
@@ -134,8 +247,7 @@ void ins(BTree* BT, int key)
                     newnode->keys[j] = node->keys[i];
                     newnode->ptrs[j] = node->ptrs[i];
                 }
-                // 1 more for ptrs[]
-                newnode->ptrs[j] = node->ptrs[i];
+                newnode->ptrs[j] = node->ptrs[i]; // once more for ptrs[]
 
                 // if node is the root, then after the split
                 // we need a new root node, which will be the
@@ -166,8 +278,6 @@ void ins(BTree* BT, int key)
     } else
         printf("%d already exists!", key);
 }
-
-void del(BTree* BT, int data) { }
 
 void main(void)
 {
