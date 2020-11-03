71ee813c833860bf3843341736f750ee3d9b4571
mountAP <ap44122@gmail.com>
2020-11-04 01:07:27 +0530

    Suspended

--- Trees/BTree.c
+++ Trees/BTree.c
@@ -4,19 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define M 5          // maximum order
-const int t = M / 2; // minimum order
+#define M 5          // maximum degree
+const int t = M / 2; // minimum degree
+// condition: t >= 2
 
 typedef struct Node {
   int n, keys[M - 1];
-  struct Node *ptrs[M];
+  struct Node *children[M];
   bool isLeaf;
 } Node;
 
 Node *newNode() {
   Node *node = (Node *)malloc(sizeof(Node));
   node->n = 0;
-  // node->isLeaf = true;
+  return node;
 }
 
 typedef struct {
@@ -26,8 +27,8 @@ typedef struct {
 BTree *createBTree() {
   BTree *T = (BTree *)malloc(sizeof(BTree));
   T->root = newNode();
-  T->root->isLeaf = false;
-  // diskWrite(*T);
+  T->root->isLeaf = true;
+  // diskWrite(T);
 
   return T;
 }
@@ -58,41 +59,88 @@ Result search(Node *node, int key) {
   }
 
   else
-    res = search(node->ptrs[i], key);
+    // diskRead(node->children[i])
+    res = search(node->children[i], key);
 
   return res;
 }
 
 void splitChild(Node *node, int index) {
   Node *right = newNode();
-  Node *left = node->ptrs[index];
+  Node *left = node->children[index];
 
   right->isLeaf = left->isLeaf;
-  right->n = t - 1;
+  right->n = t;
 
   for (int i = 0; i < t - 1; ++i)
     right->keys[i] = left->keys[i + t];
 
   if (!left->isLeaf)
     for (int i = 0; t < t; ++i)
-      right->ptrs[i] = left->ptrs[i + t];
+      right->children[i] = left->children[i + t];
 
-  left->n = t - 1;
+  left->n = t;
 
-  for (int i = node->n + 1; i >= i + 1; --i)
-    node->ptrs[i + 1] = node->ptrs[i];
+  for (int i = node->n; i >= index + 1; --i)
+    node->children[i + 1] = node->children[i];
 
-  node->ptrs[index + 1] = right;
+  node->children[index + 1] = right;
 
-  for (int i = node->n; i >= index; --i)
+  for (int i = node->n - 1; i >= index; --i)
     node->keys[i + 1] = node->keys[i];
 
   node->keys[index] = left->keys[t];
   node->n -= 1;
 
-  // diskWrite(*node);
-  // diskWrite(*left);
-  // diskWrite(*right);
+  // diskWrite(node);
+  // diskWrite(left);
+  // diskWrite(right);
 }
 
-int main() {}
+void insertNonFull(Node *node, int key) {
+  int i = node->n - 1;
+
+  if (node->isLeaf) {
+    for (; i >= 1 && key < node->keys[i]; --i)
+      node->keys[i + 1] = node->keys[i];
+
+    node->keys[i + 1] = key;
+    node->n = node->n + 1;
+    // diskWrite(node);
+
+  } else {
+    for (; i >= 0 && key < node->keys[i]; --i)
+      ;
+    i++;
+    // diskRead(node->children[i])
+    if (node->children[i]->n == M - 1) {
+      splitChild(node, i);
+      if (key > node->keys[i])
+        i++;
+    }
+    insertNonFull(node->children[i], key);
+  }
+}
+
+void insert(BTree *T, int key) {
+  Node *root = T->root;
+
+  if (root->n == M - 1) {
+    Node *node = newNode();
+    T->root = node;
+    node->isLeaf = false;
+    node->children[0] = root;
+
+    splitChild(node, 0);
+    insertNonFull(node, key);
+
+  } else
+    insertNonFull(root, key);
+}
+
+int main() {
+  BTree *T = createBTree();
+
+  for (int i = 0; i < 100; ++i)
+    insert(T, i);
+}
